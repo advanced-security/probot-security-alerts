5f5df4c0470fdb5ee7cdff8bccdf774d490ad514
Ken Muse <kenmuse@users.noreply.github.com>
2022-06-13 18:58:42 -0400

    Update codeScanningAlertDismissed.ts

--- src/events/codeScanningAlertDismissed.ts
+++ src/events/codeScanningAlertDismissed.ts
@@ -32,7 +32,8 @@ export default async function codeScanningAlertDismissed(context: Context<"code_
   * Returns a value indicating if the user is a member of the approving
   * team in the organization.
   * @param context the current execution context
-  * @returns 
+  * @returns true if the user is either an owner of the organization or
+  * a member of the approving team; otherwise, false.
   */
 async function isUserApproved(context: Context<"code_scanning_alert">) {
     const owner = context.payload.repository.owner.login;
@@ -48,7 +49,7 @@ async function isUserApproved(context: Context<"code_scanning_alert">) {
             });
 
             const role = memberships.data.role;
-            context.log.info(JSON.stringify(`The user ${user} has the role \"${role}\" in the team \"${approvingTeamName}\".`));
+            context.log.info(JSON.stringify(`The user ${user} has the role ${role} in the team ${approvingTeamName}.`));
             // The role will be "maintainer" if the user is an organization owner
             // (whether or not they are explicitly in the team). The role will be "member" 
             // if the user is explicitly included in the team. If the code has reached this
@@ -58,7 +59,7 @@ async function isUserApproved(context: Context<"code_scanning_alert">) {
         catch (e) {
             // A 404 status is returned if the user is not in the team. If there's an error
             // resolving the user or a 404, default to not allowing the user to probeed
-            context.log.info(`The user ${user} is not part of the team \"${approvingTeamName}\".`)
+            context.log.info(`The user ${user} is not part of the team ${approvingTeamName}.`)
         }
     }
 
