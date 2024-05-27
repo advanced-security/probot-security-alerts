58a929d6ac9b3ac2212aaa7ea17fb7a5d5d6d4ad
Ken Muse <kenmuse@users.noreply.github.com>
2024-05-27 18:10:12 +0000

    Reduce Functions feature image size

--- .devcontainer/features/azure-function/install.sh
+++ .devcontainer/features/azure-function/install.sh
@@ -56,13 +56,26 @@ if [ "${PLATFORM}" == "arm64" ]; then
 	if [ ! -d azure-functions-core-tools ]; then
 		git clone --depth 1 https://github.com/Azure/azure-functions-core-tools
 	fi
-	chown -R "${USERNAME}":"${USERNAME}" azure-functions-core-tools
-	cd azure-functions-core-tools/src/Azure.Functions.Cli/
-	#git checkout 4.0.5801
+
+  ## Ensure the directory is owned by the non-root user
+	## chown -R "${USERNAME}":"${USERNAME}" azure-functions-core-tools
+	
+  ## Build the project
+  cd azure-functions-core-tools/src/Azure.Functions.Cli/
+  # git checkout 4.0.5801
 	${DOTNET_ROOT}/dotnet publish -r linux-arm64 -c Release -f net8.0 --self-contained=true
+    
+  ## Clear the nuget caches to reduce the image size several from 17GB to 6GB
+  ${DOTNET_ROOT}/dotnet nuget locals all --clear
+
+  ## Copy the binaries for the tool
 	mkdir -p "${AZURE_FUNC_TOOLS_DIR}"
 	mv bin/Release/net8.0/linux-arm64/publish/* "${AZURE_FUNC_TOOLS_DIR}"
+
+  ## Cleanup the source code/build
 	rm -rf "${FXN_BUILD_ROOT_DIR}/azure-functions-core-tools"
+
+  ## Update the environment variable
 	updaterc "export AZURE_FUNC_TOOLS_DIR=${AZURE_FUNC_TOOLS_DIR}"
 else
   # sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
