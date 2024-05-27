da22eef7a01284f9a4b1eb7b0f2c4ecbab87b50f
Ken Muse <kenmuse@users.noreply.github.com>
2024-05-27 19:18:10 +0000

    Azure Functions version specification

--- .devcontainer/features/azure-function/install.sh
+++ .devcontainer/features/azure-function/install.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 if [ "$(id -u)" -ne "0"  ]; then echo "Must be run as root or with sudo"; exit 1; fi
 export DEBIAN_FRONTEND=noninteractive
 UPDATE_RC=${UPDATE_RC:-"true"}
-AZURE_FUNC_TOOLS_DIR=${AZURE_FUNC_TOOLS_DIR:-"/usr/local/functools"}
+AZURE_FUNC_TOOLS_DIR=${AZURE_FUNC_TOOLS_DIR:-"/lib/azure-functions-core-tools-4"}
 USERNAME=${USERNAME:-"automatic"}
+VERSION=${VERSION:-"latest"}
+TARGET_SDK=net8.0
 
 updaterc() {
     if [ "${UPDATE_RC}" = "true" ]; then
@@ -21,64 +23,52 @@ updaterc() {
 }
 
 # Ensure that login shells get the correct path if the user updated the PATH using ENV.
-rm -f /etc/profile.d/00-func-restore-env.sh
-echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-func-restore-env.sh
-chmod +x /etc/profile.d/00-func-restore-env.sh
-
-# Determine the appropriate non-root user
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-        if id -u "${CURRENT_USER}" > /dev/null 2>&1; then
-            USERNAME="${CURRENT_USER}"
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=root
-    fi
-elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
-    USERNAME=root
-fi
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
 
 PROCESSOR_ARCHITECTURE=$(uname -m)
 if [ "${PROCESSOR_ARCHITECTURE}" == "arm64" ] || [ "${PROCESSOR_ARCHITECTURE}" == "aarch64" ]; then
     declare -r PLATFORM=arm64
+    declare -r RID_PLATFORM=arm64
 else
     declare -r PLATFORM=amd64
+    declare -r RID_PLATFORM=x64
 fi
 
-if [ "${PLATFORM}" == "arm64" ]; then
-    FXN_BUILD_ROOT_DIR=/tmp
-    # [ -d "${FXN_BUILD_ROOT_DIR}" ] || mkdir -p "${FXN_BUILD_ROOT_DIR}"
-    cd "${FXN_BUILD_ROOT_DIR}"
-	if [ ! -d azure-functions-core-tools ]; then
-		git clone --depth 1 https://github.com/Azure/azure-functions-core-tools
+if [ "${PLATFORM}" == "arm64" ] || [ "${VERSION}" != "latest" ]; then
+  ## Create a temporary space 
+  FXN_BUILD_ROOT_DIR=`mktemp -d`
+  cd "${FXN_BUILD_ROOT_DIR}"
+	
+  ## Download the source code for the requested version (branch/tag)
+  if [ ! -d azure-functions-core-tools ]; then
+		if [ "${VERSION}" == "latest" ]; then
+			BRANCH=v4.x
+		else
+		  BRANCH=${VERSION}
+		fi
+		git clone --depth 1 --branch "${BRANCH}" https://github.com/Azure/azure-functions-core-tools
 	fi
-
-  ## Ensure the directory is owned by the non-root user
-	## chown -R "${USERNAME}":"${USERNAME}" azure-functions-core-tools
 	
   ## Build the project
   cd azure-functions-core-tools/src/Azure.Functions.Cli/
-  # git checkout 4.0.5801
-	${DOTNET_ROOT}/dotnet publish -r linux-arm64 -c Release -f net8.0 --self-contained=true
+	${DOTNET_ROOT}/dotnet publish -r "linux-${RID_PLATFORM}" -c Release -f "${TARGET_SDK}" --self-contained=true
     
   ## Clear the nuget caches to reduce the image size several from 17GB to 6GB
   ${DOTNET_ROOT}/dotnet nuget locals all --clear
 
   ## Copy the binaries for the tool
 	mkdir -p "${AZURE_FUNC_TOOLS_DIR}"
-	mv bin/Release/net8.0/linux-arm64/publish/* "${AZURE_FUNC_TOOLS_DIR}"
+  [ -d "${AZURE_FUNC_TOOLS_DIR}" ] || mkdir -p "${AZURE_FUNC_TOOLS_DIR}"
+	mv bin/Release/${TARGET_SDK}/linux-${RID_PLATFORM}/publish/* "${AZURE_FUNC_TOOLS_DIR}"
 
   ## Cleanup the source code/build
-	rm -rf "${FXN_BUILD_ROOT_DIR}/azure-functions-core-tools"
+	rm -rf "${FXN_BUILD_ROOT_DIR}"
 
   ## Update the environment variable
 	updaterc "export AZURE_FUNC_TOOLS_DIR=${AZURE_FUNC_TOOLS_DIR}"
 else
-  # sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
 	sudo apt-get update
-	sudo apt-get install -y azure-functions-core-tools-4 dotnet-sdk-8.0
+	sudo apt-get install -y azure-functions-core-tools-4
 fi
