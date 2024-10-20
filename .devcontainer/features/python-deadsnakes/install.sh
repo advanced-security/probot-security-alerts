4e7698c8420b30794763c35559e7e5372aca0084
Ken Muse <kenmuse@users.noreply.github.com>
2024-10-20 18:45:31 +0000

    Install prebuilt Python binaries

---
+++ .devcontainer/features/python-deadsnakes/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PYTHONVERSION=${PYTHONVERSION:-"3.13"}
+
+if [ "$(id -u)" -ne "0"  ]; then echo "Must be run as root or with sudo"; exit 1; fi
+export DEBIAN_FRONTEND=noninteractive
+
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update -qq 
+apt-get install -y -qq python${PYTHONVERSION} > /dev/null
+
+# Clean up
+apt clean
+rm -rf /var/lib/apt/lists/*
